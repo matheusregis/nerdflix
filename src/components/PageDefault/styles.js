fff6f2726a6c0b6f0918887d636a3a078d4a11fe
Matheus Régis <matheustrai@gmail.com>
2020-07-28 20:40:45 -0300

    Padronizado Header/footer, criado page Categoria

---
+++ src/components/PageDefault/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Main = styled.main `
+  background-color: var(--black);
+  color: var(--white);
+  flex: 1;
+  padding-top: 50px;
+  padding-right: 5%;
+  padding-left: 5%;
+`;
